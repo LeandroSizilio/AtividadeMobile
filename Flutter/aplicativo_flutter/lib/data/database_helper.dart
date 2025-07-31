abc7d366a47f864593dd277b56a286d95cbe4c7d
Felipe-CF <prog.study.fcf@gmail.com>
2025-07-31 16:00:28 -0300

    tentativas de fazer funcionar...

---
+++ Flutter/aplicativo_flutter/lib/data/database_helper.dart
@@ -0,0 +1,55 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('app.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dir = await getApplicationDocumentsDirectory();
+    final path = join(dir.path, fileName);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE fichas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        str INTEGER,
+        dex INTEGER,
+        con INTEGER,
+        inteli INTEGER,
+        wis INTEGER,
+        hp INTEGER,
+        mana INTEGER,
+        nome TEXT NOT NULL,
+        race TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<void> verificarBanco() async {
+    final db = await database;
+    final resultado = await db.rawQuery(
+      "SELECT name FROM sqlite_master WHERE type='table'"
+    );
+
+    print('Tabelas no banco:');
+    for (var row in resultado) {
+      print(row['name']);
+    }
+  }
+}
+
+
---
+++ Flutter/aplicativo_flutter/lib/data/models/ficha.dart
@@ -0,0 +1,38 @@
+class Ficha{
+  final int? id;
+
+  final int? str; final int? dex; final int? con; 
+  final int? inteli; final int? wis; 
+
+  final int? hp; final int? mana; 
+  
+  final String nome;
+  final String race;
+  
+  // init
+  Ficha({
+    this.id, this.str, this.dex, this.con, this.inteli, this.wis,
+    this.hp, this.mana, 
+    required this.nome, required this.race
+    });
+
+  // Convertendo para Map (para salvar no banco)
+  Map<String, dynamic> toMap(){
+    return{
+      'id': id,
+      'str': str, 'dex': dex, 'con': con, 'inteli': inteli, 'wis': wis,
+      'hp': hp, 'mana': mana,
+      'nome': nome, 'race': race,
+    };
+  }
+
+  // Convertendo do Map (quando buscano banco)
+  factory Ficha.fromMap(Map<String, dynamic> map){
+    return Ficha(
+      nome: map['nome'], race: map['race'],
+      str: map['str'], dex: map['dex'], con: map['con'], inteli: map['inteli'], wis: map['wis'], 
+      hp: map['hp'], mana: map['mana'],
+    );
+  }
+ 
+}
